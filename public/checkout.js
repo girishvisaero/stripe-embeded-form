ea4f8a4929d66412d17c26f6eb9db730b02ff787
girish.chaudhari <girish.chaudhari@visaero.com>
2024-05-08 12:48:15 +0530

    added project

---
+++ public/checkout.js
@@ -0,0 +1,24 @@
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+const stripe = Stripe("pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3");
+
+initialize();
+
+// Create a Checkout Session
+async function initialize() {
+  const fetchClientSecret = async () => {
+    const response = await fetch("http://localhost:4242/create-checkout-session", {
+      method: "POST",
+    });
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+  });
+
+  // Mount Checkout
+  checkout.mount('#checkout');
+}
